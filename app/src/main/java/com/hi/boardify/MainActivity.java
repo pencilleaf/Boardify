50d59b7ee4adf2af56d7105b1ac2f59f87f2925b
Sean Lew <seanlewtengsiong@gmail.com>
2018-12-06 14:37:46 +0800

    Color Icons

--- app/src/main/java/com/hi/boardify/MainActivity.java
+++ app/src/main/java/com/hi/boardify/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
     }
 
     @Override
